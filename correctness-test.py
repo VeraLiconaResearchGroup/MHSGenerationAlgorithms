d29ddb935daec1ad1401fd266a813b9fd38995e5
Andrew Gainer-Dewar <andrew.gainer.dewar@gmail.com>
2015-09-23 16:05:40 -0400

    Rewrite correctness-test.py to reuse containers

--- correctness-test.py
+++ correctness-test.py
@@ -48,18 +48,13 @@ correct_transversals = frozenset(frozenset(transversal) for transversal in test_
 if not args.slow:
     alg_list = filter(lambda alg: not alg.get("slow"), alg_list)
 
-# Split out threading cases if requested
-if args.num_threads is not None:
-    thread_algs = filter(lambda alg: alg.get("threads"), alg_list)
-    for alg in thread_algs:
-        alg_list.remove(alg)
-        for n in args.num_threads:
-            newalg = copy.deepcopy(alg)
-            newalg["algName"] = "{0}-t{1}".format(alg.get("algName"), n)
-            if newalg.get("config") is None:
-                newalg["config"] = {}
-                newalg["config"]["THREADS"] = n
-            alg_list.append(newalg)
+# Filter out non-threading algorithms if appropriate
+num_threads = args.num_threads
+if num_threads is None:
+    num_threads = [1]
+
+if 1 not in num_threads:
+    alg_list = filter(lambda alg: alg.get("threads"), alg_list)
 
 # Launch containers
 logging.info("Launching containers")
@@ -69,33 +64,38 @@ alg_collection = pyalgorun.AlgorunContainerCollection(alg_list, docker_base_url 
 tests_failed = []
 
 logging.info("Running algorithms")
-results = alg_collection.run_all_with_input(input_str)
-for algname, algresult in results.iteritems():
-    # Run the algorithm
-    try:
-        result = json.loads(algresult)
-    except ValueError:
-        errormessage = "Algorithm {0} returned invalid JSON: {1}".format(algname, algresult)
-        raise RuntimeError(errormessage)
+for alg in alg_collection:
+    # Only iterate over cases supported by the algorithm
+    alg_entry = next(entry for entry in alg_list if entry["algName"] == alg._name)
+    alg_thread_list = num_threads if alg_entry.get("threads") else [1]
 
-    logging.debug("Algorithm {0} finished.".format(algname))
+    for t in alg_thread_list:
+        newname = "{0}-t{1}".format(alg._name, t)
+        try:
+            result = json.loads(alg.run_alg(input_str))
+        except ValueError:
+            errormessage = "Algorithm {0} returned invalid JSON: {1}".format(newname, algresult)
+            raise RuntimeError(errormessage)
 
-    # Store the transversals as sets in a set for easy equality testing
-    transversals = frozenset(frozenset(transversal) for transversal in result["transversals"])
+        # Store the transversals as sets in a set for easy equality testing
+        transversals = frozenset(frozenset(transversal) for transversal in result["transversals"])
 
-    # Test the result for correctness and write diagnostics in case of failure
-    if transversals != correct_transversals:
-        false_includes = list(list(transversal) for transversal in transversals.difference(correct_transversals))
-        false_excludes = list(list(transversal) for transversal in correct_transversals.difference(transversals))
+        # Test the result for correctness and write diagnostics in case of failure
+        if transversals != correct_transversals:
+            logging.debug("Algorithm {0} failed!".format(newname))
+            false_includes = list(list(transversal) for transversal in transversals.difference(correct_transversals))
+            false_excludes = list(list(transversal) for transversal in correct_transversals.difference(transversals))
 
-        errors = {
-            "false_excludes": false_excludes,
-            "false_includes": false_includes
-        }
+            errors = {
+                "false_excludes": false_excludes,
+                "false_includes": false_includes
+            }
 
-        result = {"algName": algname, "errors": errors}
+            result = {"algName": newname, "errors": errors}
 
-        tests_failed.append(result)
+            tests_failed.append(result)
+        else:
+            logging.debug("Algorithm {0} passed.".format(newname))
 
 if len(tests_failed) == 0:
     logging.warning("All tests succeded.")
