9953f29977b875d40181b9b452e254437c45e2cd
Andrew Gainer-Dewar <andrew.gainer.dewar@gmail.com>
2015-09-24 14:08:45 -0400

    Use CAND optimization in pRS

--- algorithms/agdmhs/src/alg/src/rs.cpp
+++ algorithms/agdmhs/src/alg/src/rs.cpp
@@ -47,19 +47,39 @@ namespace agdmhs {
     static void rs_extend_or_confirm_set(const Hypergraph& H,
                                          const Hypergraph& T,
                                          const bitset& S,
+                                         const bitset& CAND,
                                          const Hypergraph& crit,
                                          const bitset& uncov,
                                          const size_t cutoff_size) {
         // Input specification
         assert(uncov.any()); // uncov cannot be empty
+        assert(CAND.any()); // CAND cannot be empty
         assert(cutoff_size == 0 or S.count() < cutoff_size); // If we're using a cutoff, S must not be too large
 
         // Otherwise, get an uncovered edge
-        hindex i = uncov.find_first(); // Just use the first set in uncov
+        hindex search_edge = uncov.find_first(); // Per M+U, we use the first edge (to ensure proper search order)
+        bitset e = H[search_edge];
+        while (search_edge != bitset::npos) {
+            if ((H[search_edge] & CAND).count() < (e & CAND).count()) {
+                e = H[search_edge];
+            }
+            search_edge = uncov.find_next(search_edge);
+        }
 
-        // Loop over vertices in that edge
-        hindex v = H[i].find_first();
+        // Then consider vertices lying in the intersection of e with CAND
+        bitset C = CAND & e; // intersection
+        bitset newCAND = CAND & (~e); // difference
+
+        // Store the indices in C in descending order for iteration
+        std::deque<hindex> Cindices;
+        hindex v = C.find_first();
         while (v != bitset::npos) {
+            Cindices.push_front(v);
+            v = C.find_next(v);
+        }
+
+        // Test all the vertices in C (in descending order)
+        for (auto& v: Cindices) {
             // Check preconditions
             Hypergraph new_crit = crit;
             bitset new_uncov = uncov;
@@ -67,12 +87,12 @@ namespace agdmhs {
                 update_crit_and_uncov(new_crit, new_uncov, H, T, S, v);
             }
             catch (vertex_violating_exception& e) {
-                v = H[i].find_next(v);
+                newCAND.set(v);
                 continue;
             }
 
-            if (rs_any_edge_critical_after_i(i, S, new_crit)) {
-                v = H[i].find_next(v);
+            if (rs_any_edge_critical_after_i(search_edge, S, new_crit)) {
+                newCAND.set(v);
                 continue;
             }
 
@@ -82,18 +102,15 @@ namespace agdmhs {
 
             if (new_uncov.none()) {
                 HittingSets.enqueue(newS);
-                v = H[i].find_next(v);
-                continue;
-            }
-
+            } else if (cutoff_size == 0 or newS.count() < cutoff_size) {
             // After this point, we'll be considering extending newS even more.
             // If we're using a cutoff, this requires more room.
-            if (cutoff_size == 0 or newS.count() < cutoff_size) {
 #pragma omp task untied shared(H, T)
-                rs_extend_or_confirm_set(H, T, newS, new_crit, new_uncov, cutoff_size);
+                rs_extend_or_confirm_set(H, T, newS, newCAND, new_crit, new_uncov, cutoff_size);
             }
 
-            v = H[i].find_next(v);
+            // Update newCAND and proceed to new vertex
+            newCAND.set(v);
         }
     };
 
@@ -107,6 +124,10 @@ namespace agdmhs {
         // Candidate hitting set
         bitset S (H.num_verts());
         S.reset(); // Initially empty
+
+        // Eligible vertices
+        bitset CAND (H.num_verts());
+        CAND.set(); // Initially full
 
         // Which edges each vertex is critical for
         Hypergraph crit (H.num_edges(), H.num_verts());
@@ -122,7 +143,7 @@ namespace agdmhs {
         {
 #pragma omp parallel shared(H, T)
 #pragma omp single
-            rs_extend_or_confirm_set(H, T, S, crit, uncov, cutoff_size);
+            rs_extend_or_confirm_set(H, T, S, CAND, crit, uncov, cutoff_size);
 #pragma omp taskwait
         }
 
