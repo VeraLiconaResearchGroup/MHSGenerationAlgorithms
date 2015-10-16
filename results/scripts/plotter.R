7b0bea65bb48126760c69db0eca55628bf2bd36f
Andrew Gainer-Dewar <andrew.gainer.dewar@gmail.com>
2015-10-16 16:07:49 -0400

    Update plotter.R style

--- results/scripts/plotter.R
+++ results/scripts/plotter.R
@@ -1,49 +1,63 @@
-# Set up scales
-yscale = scale_y_log10(limits = c(0.001, 3600), breaks=c(0.001, 0.01, 0.1, 1, 10, 100, 1000))
-ylabel = ylab("Runtime (s., log. scale)")
-yticks = annotation_logticks(side="lr")
+## Set up scales
+yscale <- scale_y_log10(limits = c(0.001, 3600), breaks=c(0.001, 0.01, 0.1, 1, 10, 100, 1000), oob = scales::rescale_none)
+ylabel <- ylab("Runtime (s., log. scale)")
+yticks <- annotation_logticks(side="lr")
 
-# Build a plot of full run data
+label_rotator <- theme(axis.text.x = element_text(angle = 30, hjust = 1))
+
+## Replace Inf values in a dataset with huge finite values for charting
+blowup_inf <- function(data){
+    data[ data == Inf ] <- (3600 * 10^4)
+    return(data)
+}
+
+## Build a plot of full run data
 plot_full <- function(data){
-    # Lay out x scale
-    xscale_full = scale_x_discrete()
+    ## Lay out x scale
+    xscale_full <- scale_x_discrete()
 
-    # Format labels on x axis
-    x_label_rotator = theme(axis.text.x = element_text(angle = 30, hjust = 1))
+    ## Clean up infinite values
+    data <- blowup_inf(data)
 
-    # Melt data
+    ## Melt data
     mdf <- melt(data, id.vars='Input', variable.name = "Algorithm", value.name = "Runtime")
 
-    # Construct plot
-    dataplot = ggplot(data=mdf) + geom_line(aes(linetype = Algorithm, x = Input, y = Runtime, group = Algorithm)) + xscale_full +  x_label_rotator + yscale + yticks + ylabel
+    ## Construct plot
+    dataplot <- ggplot(data=mdf) + geom_line(aes(color = Algorithm, x = Input, y = Runtime, group = Algorithm)) + xscale_full + yscale + yticks + ylabel + label_rotator
 
     return(dataplot)
 }
 
-# Build a plot of cutoff data
+## Build a plot of cutoff data
 plot_cutoff <- function(data){
-    # Lay out x scale
-    xscale_cutoff = scale_x_discrete(limits = c(5, 7, 10, 'None'))
+    ## Lay out x scale
+    xscale_cutoff <- scale_x_discrete(limits = c(5, 7, 10, 'None'))
 
-    # Melt data
+    ## Clean up infinite values
+    data <- blowup_inf(data)
+    
+    ## Melt data
     mdf <- melt(data, id.vars='Cutoff', variable.name = "Algorithm", value.name = "Runtime")
 
-    # Construct plot
-    dataplot = ggplot(data=mdf) + geom_line(aes(linetype = Algorithm, x = Cutoff, y = Runtime, group = Algorithm)) + xscale_cutoff + yscale + yticks + ylabel
+    ## Construct plot
+    dataplot <- ggplot(data=mdf) + geom_line(aes(color = Algorithm, x = Cutoff, y = Runtime, group = Algorithm)) + xscale_cutoff + yscale + yticks + ylabel
 
     return(dataplot)
 }
 
-# Build a plot of threaded data
+## Build a plot of threaded data
 plot_threaded <- function(data){
-    # Lay out x scale
-    xscale_threads = scale_x_discrete(limits = c(1, 2, 4, 8, 12, 16))
+    ## Lay out x scale
+    xscale_threads <- scale_x_discrete(limits = c(1, 2, 4, 6, 8, 12, 16))
 
-    # Melt data
+    ## Clean up infinite values
+    data <- blowup_inf(data)
+    
+    ## Melt data
     mdf <- melt(data, id.vars='Threads', variable.name = "Algorithm", value.name = "Runtime")
 
-    # Construct plot
-    dataplot = ggplot(data=mdf) + geom_line(aes(linetype = Algorithm, x = Threads, y = Runtime, group = Algorithm)) + xscale_threads + yscale + yticks + ylabel
+    ## Construct plot
+    dataplot <- ggplot(data=mdf) + geom_line(aes(color = Algorithm, x = Threads, y = Runtime, group = Algorithm)) + xscale_threads + yscale + yticks + ylabel
 
     return(dataplot)
 }
