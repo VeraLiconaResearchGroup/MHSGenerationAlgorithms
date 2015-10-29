ca50497b3a58ca269b0b96ea687e39a379a06175
Andrew Gainer-Dewar <andrew.gainer.dewar@gmail.com>
2015-10-29 11:43:54 -0400

    Quick modularlization of benchmark.py

--- benchmark.py
+++ benchmark.py
@@ -16,98 +16,19 @@ from collections import defaultdict
 
 MAX_TRIES = 1
 
-def main():
-    # Set up argument processing
-    parser = argparse.ArgumentParser(description="MHS algorithm benchmark runner")
-
-    # Suppress log noise from requests library
-    logging.getLogger("requests").setLevel(logging.WARNING)
-
-    # Add arguments
-    parser.add_argument("algorithm_list_file", help="JSON file of algorithms to benchmark")
-    parser.add_argument("input_data_file", help="Input file to be passed to each algorithm")
-    parser.add_argument("output_dir", help="Output directory to write results")
-    parser.add_argument("-n", "--num_tests", type=int, default=1, help="Number of test iterations")
-    parser.add_argument("-j", "--num_threads", type=int, nargs='*', help="Numbers of threads to use for supporting algorithms")
-    parser.add_argument("-c", "--cutoff_sizes", type=int, nargs='*', help="Cutoff sizes to use for supporting algorithms (if specified, full test is not run unless 0 is included)")
-    parser.add_argument("-t", "--timeout", type=int, default=0, help="Kill algorithms after this many seconds (0 to run forever)")
-    parser.add_argument("-d", "--docker_base_url", default=None, help="Base URL for Docker client")
-    parser.add_argument('-v', '--verbose', action="count", default=0, help="Print verbose logs (may be used multiple times)")
-    parser.add_argument('-s', '--slow', action="store_true", help="Include slow algorithms (be careful!)")
-    parser.add_argument('-a', '--append', action="store_true", help="Append results to existing file, replacing repeated algorithms (otherwise, overwrite!)")
-
-    # Process the arguments
-    args = parser.parse_args()
-
-    # Set up logging
-    log_format = '%(levelname)s [%(asctime)s] %(message)s'
-
-    if args.verbose == 0:
-        log_level = logging.WARNING
-    elif args.verbose == 1:
-        log_level = logging.INFO
-    else:
-        log_level = logging.DEBUG
-
-    logging.basicConfig(format = log_format, level = log_level)
-
-    infile_basename = os.path.splitext(os.path.basename(args.input_data_file))[0]
-    logfile_path = "{0}/{1}.log".format(args.output_dir, infile_basename)
-
-    logfile_handler = logging.FileHandler(logfile_path)
-    logging.getLogger().addHandler(logfile_handler)
-
-    # Read and process files
-    with open(args.algorithm_list_file) as algorithm_list_file:
-        alg_list = json.load(algorithm_list_file)["containers"]
-
-    # Filter out slow algorithms if requested
-    if not args.slow:
-        alg_list = filter(lambda alg: not alg.get("slow"), alg_list)
-
-    # Filter out non-threading algorithms if appropriate
-    num_threads = args.num_threads
-    if num_threads is None:
-        num_threads = [1]
-
-    if 1 not in num_threads:
-        alg_list = filter(lambda alg: alg.get("threads"), alg_list)
-
-    num_threads.sort(reverse=True)
-
-    # Filter out non-cutoff algorithms if appropriate
-    cutoff_sizes = args.cutoff_sizes
-    if cutoff_sizes is None:
-        cutoff_sizes = [0]
-
-    cutoff_sizes.sort()
-
-    if 0 in cutoff_sizes:
-        # Put 0 at the end of the list so the timeout filtering logic works
-        cutoff_sizes.remove(0)
-        cutoff_sizes.append(0)
-    else:
-        # Remove algorithms that don't support cutoff
-        alg_list = filter(lambda alg: alg.get("cutoff"), alg_list)
-
-    # Process timeout
-    timeout = args.timeout
-    if timeout == 0:
-        timeout = None
-
-    # Process filenames
-    output_data_filename = "{0}/{1}.json".format(args.output_dir, infile_basename)
-
-    alg_results_dirname = "{0}/alg-output/".format(args.output_dir)
-    try:
-        os.makedirs(alg_results_dirname)
-    except OSError:
-        if not os.path.isdir(alg_results_dirname):
-            raise
-
+def run_benchmarks(alg_list,
+                   docker_base_url,
+                   input_data_filename,
+                   output_data_filename,
+                   alg_results_dirname,
+                   num_tests,
+                   cutoff_list,
+                   threads_list,
+                   timeout,
+                   append = False):
     # Launch containers
     logging.info("Launching containers")
-    alg_collection = pyalgorun.AlgorunContainerCollection(alg_list, docker_base_url = args.docker_base_url)
+    alg_collection = pyalgorun.AlgorunContainerCollection(alg_list, docker_base_url = docker_base_url)
 
     # Set up dicts to store the results
     runtimes = defaultdict(list)
@@ -119,7 +40,7 @@ def main():
     original_algs = []
     original_timeout = timeout
 
-    if args.append:
+    if append:
         try:
             with open(output_data_filename) as output_data_file:
                 original_output = json.load(output_data_file)
@@ -142,8 +63,8 @@ def main():
     for alg in alg_collection:
         # Only iterate over cases supported by the algorithm
         alg_entry = next(entry for entry in alg_list if entry["algName"] == alg._name)
-        alg_thread_list = num_threads if alg_entry.get("threads") else [1]
-        alg_cutoff_list = cutoff_sizes if alg_entry.get("cutoff") else [0]
+        alg_thread_list = threads_list if alg_entry.get("threads") else [1]
+        alg_cutoff_list = cutoff_list if alg_entry.get("cutoff") else [0]
 
         timeout_config_pairs = []
 
@@ -154,7 +75,7 @@ def main():
                 num_tries = 0
 
 
-                for i in range(args.num_tests):
+                for i in range(num_tests):
                     # NOTE: We assume that increasing the cutoff or
                     # decreasing the number of threads will never decrease
                     # runtime. This only matters for runtimes that move
@@ -193,12 +114,12 @@ def main():
                     # Only execute this run if a faster configuration
                     # has not timed out
                     if not alg_has_timed_out:
-                        logging.info("Running algorithm {0} with {1} threads and cutoff size {2}, run {3}/{4}".format(alg, t, c, i+1, args.num_tests))
+                        logging.info("Running algorithm {0} with {1} threads and cutoff size {2}, run {3}/{4}".format(alg, t, c, i+1, num_tests))
                         config = {"THREADS": t, "CUTOFF_SIZE": c}
                         alg.change_config(config)
 
                         try:
-                            result_str = alg.run_alg(args.input_data_file, timeout)
+                            result_str = alg.run_alg(input_data_filename, timeout)
                             result = json.loads(result_str)
                             time_taken = float(result["timeTaken"])
                             transcount = len(result["transversals"])
@@ -262,9 +183,115 @@ def main():
         "algs": alg_list,
     }
 
+    return output
+
+
+def main():
+    # Set up argument processing
+    parser = argparse.ArgumentParser(description="MHS algorithm benchmark runner")
+
+    # Suppress log noise from requests library
+    logging.getLogger("requests").setLevel(logging.WARNING)
+
+    # Add arguments
+    parser.add_argument("algorithm_list_file", help="JSON file of algorithms to benchmark")
+    parser.add_argument("input_data_file", help="Input file to be passed to each algorithm")
+    parser.add_argument("output_dir", help="Output directory to write results")
+    parser.add_argument("-n", "--num_tests", type=int, default=1, help="Number of test iterations")
+    parser.add_argument("-j", "--num_threads", type=int, nargs='*', help="Numbers of threads to use for supporting algorithms")
+    parser.add_argument("-c", "--cutoff_sizes", type=int, nargs='*', help="Cutoff sizes to use for supporting algorithms (if specified, full test is not run unless 0 is included)")
+    parser.add_argument("-t", "--timeout", type=int, default=0, help="Kill algorithms after this many seconds (0 to run forever)")
+    parser.add_argument("-d", "--docker_base_url", default=None, help="Base URL for Docker client")
+    parser.add_argument('-v', '--verbose', action="count", default=0, help="Print verbose logs (may be used multiple times)")
+    parser.add_argument('-s', '--slow', action="store_true", help="Include slow algorithms (be careful!)")
+    parser.add_argument('-a', '--append', action="store_true", help="Append results to existing file, replacing repeated algorithms (otherwise, overwrite!)")
+
+    # Process the arguments
+    args = parser.parse_args()
+
+    # Set up logging
+    log_format = '%(levelname)s [%(asctime)s] %(message)s'
+
+    if args.verbose == 0:
+        log_level = logging.WARNING
+    elif args.verbose == 1:
+        log_level = logging.INFO
+    else:
+        log_level = logging.DEBUG
+
+    logging.basicConfig(format = log_format, level = log_level)
+
+    infile_basename = os.path.splitext(os.path.basename(args.input_data_file))[0]
+    logfile_path = "{0}/{1}.log".format(args.output_dir, infile_basename)
+
+    logfile_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    logfile_handler = logging.FileHandler(logfile_path)
+    logfile_handler.setFormatter(logfile_formatter)
+    logging.getLogger().addHandler(logfile_handler)
+
+    # Read and process files
+    with open(args.algorithm_list_file) as algorithm_list_file:
+        alg_list = json.load(algorithm_list_file)["containers"]
+
+    # Filter out slow algorithms if requested
+    if not args.slow:
+        alg_list = filter(lambda alg: not alg.get("slow"), alg_list)
+
+    # Filter out non-threading algorithms if appropriate
+    threads_list = args.num_threads
+    if threads_list is None:
+        threads_list = [1]
+
+    if 1 not in threads_list:
+        alg_list = filter(lambda alg: alg.get("threads"), alg_list)
+
+    threads_list.sort(reverse=True)
+
+    # Filter out non-cutoff algorithms if appropriate
+    cutoff_list = args.cutoff_sizes
+    if cutoff_list is None:
+        cutoff_list = [0]
+
+    cutoff_list.sort()
+
+    if 0 in cutoff_list:
+        # Put 0 at the end of the list so the timeout filtering logic works
+        cutoff_list.remove(0)
+        cutoff_list.append(0)
+    else:
+        # Remove algorithms that don't support cutoff
+        alg_list = filter(lambda alg: alg.get("cutoff"), alg_list)
+
+    # Process timeout
+    timeout = args.timeout
+    if timeout == 0:
+        timeout = None
+
+    # Process filenames
+    output_data_filename = "{0}/{1}.json".format(args.output_dir, infile_basename)
+
+    alg_results_dirname = "{0}/alg-output/".format(args.output_dir)
+    try:
+        os.makedirs(alg_results_dirname)
+    except OSError:
+        if not os.path.isdir(alg_results_dirname):
+            raise
+
+    # Run the benchmarks
+    results = run_benchmarks(alg_list,
+                             args.docker_base_url,
+                             args.input_data_file,
+                             output_data_filename,
+                             alg_results_dirname,
+                             args.num_tests,
+                             cutoff_list,
+                             threads_list,
+                             timeout,
+                             args.append)
+
     # Print the results
     with open(output_data_filename, 'w') as output_data_file:
-        json.dump(output, output_data_file, indent=4, separators=(',', ': '), sort_keys = True) # Pretty-print the output
+        json.dump(results, output_data_file, indent=4, separators=(',', ': '), sort_keys = True) # Pretty-print the output
 
 if __name__ == "__main__":
     main()
