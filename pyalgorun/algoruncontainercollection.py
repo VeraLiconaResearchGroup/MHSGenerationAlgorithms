66b0f16cd0634099940a2370aca313ec4a5182c2
Andrew Gainer-Dewar <andrew.gainer.dewar@gmail.com>
2015-11-06 13:56:16 -0500

    GPLv3 for scripts in main project

--- pyalgorun/algoruncontainercollection.py
+++ pyalgorun/algoruncontainercollection.py
@@ -4,6 +4,18 @@
 # Copyright Vera-Licona Research Group (C) 2015
 # Author: Andrew Gainer-Dewar, Ph.D. <andrew.gainer.dewar@gmail.com>
 
+# This file is part of MHSGenerationAlgorithms.
+
+# MHSGenerationAlgorithms is free software: you can redistribute it
+# and/or modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation, either version 3 of
+# the License, or (at your option) any later version.
+#
+# MHSGenerationAlgorithms is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+
 from algoruncontainer import AlgorunContainer
 import json
 import jsonschema
