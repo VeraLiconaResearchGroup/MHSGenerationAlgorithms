cecbd1327a3cd4de24a9ed61b09cef95e27d9530
Andrew Gainer-Dewar <andrew.gainer.dewar@gmail.com>
2015-11-06 15:04:46 -0500

    Update licensing for OCSANA MHS

--- containers/ocsanamhs/src/alg/hittingSetGreedyAlgorithm.java
+++ containers/ocsanamhs/src/alg/hittingSetGreedyAlgorithm.java
@@ -1,5 +1,6 @@
 /*
- * Copyright (C) 2015 misagh.kordi
+ * Copyright (C) 2015 Vera-Licona Research Group
+ * Author: Misagh Kordi
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
