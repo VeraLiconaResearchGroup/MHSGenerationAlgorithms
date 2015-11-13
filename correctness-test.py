d481fb823f8690c02b561aca9bf25d0564b74702
Andrew Gainer-Dewar <andrew.gainer.dewar@gmail.com>
2015-11-13 09:58:44 -0500

    Fix leftover bugs in correctness-test

--- correctness-test.py
+++ correctness-test.py
@@ -29,9 +29,9 @@ parser = argparse.ArgumentParser(description="MHS algorithm correctness tester")
 logging.getLogger("requests").setLevel(logging.WARNING)
 
 # Add arguments
-parser.add_argument("algorithm_list_file", type=argparse.FileType('r'), help="JSON file of algorithms to benchmark")
-parser.add_argument("test_data_file", type=argparse.FileType('r'), help="Test input file containing both sets and transversals")
-parser.add_argument("errors_file", type=argparse.FileType('w'), help="File to write errors in JSON format")
+parser.add_argument("algorithm_list_file", help="JSON file of algorithms to benchmark")
+parser.add_argument("test_data_file", help="Test input file containing both sets and transversals")
+parser.add_argument("errors_file", help="File to write errors in JSON format")
 parser.add_argument("-j", dest="num_threads", type=int, nargs='*', help="Number of threads to use for supporting algorithms")
 parser.add_argument("-d", dest="docker_base_url", default=None, help="Base URL for Docker client")
 parser.add_argument('-v', '--verbose', action="count", default=0, help="Print verbose logs (may be used multiple times)")
@@ -53,12 +53,12 @@ else:
 logging.basicConfig(format = log_format, level = log_level)
 
 # Read and process files
-alg_list = json.load(args.algorithm_list_file)["containers"]
+with open(args.algorithm_list_file) as alg_list_file:
+    alg_list = json.load(alg_list_file)["containers"]
 
-test_data_dict = json.load(args.test_data_file)
-input_str = json.dumps(test_data_dict)
-
-correct_transversals = frozenset(frozenset(transversal) for transversal in test_data_dict["transversals"])
+with open(args.test_data_file) as test_data_file:
+    test_data_dict = json.load(test_data_file)
+    correct_transversals = frozenset(frozenset(transversal) for transversal in test_data_dict["transversals"])
 
 # Filter out slow algorithms if requested
 if not args.slow:
@@ -88,7 +88,8 @@ for alg in alg_collection:
     for t in alg_thread_list:
         newname = "{0}-t{1}".format(alg._name, t)
         try:
-            result = json.loads(alg.run_alg(input_str))
+            result_str = alg.run_alg(args.test_data_file, 600)
+            result = json.loads(result_str)
         except ValueError:
             errormessage = "Algorithm {0} returned invalid JSON: {1}".format(newname, algresult)
             raise RuntimeError(errormessage)
@@ -117,9 +118,6 @@ if len(tests_failed) == 0:
     logging.warning("All tests succeded.")
 else:
     result = {"correctTransversals": test_data_dict["transversals"], "algErrors": tests_failed}
-    logging.warning("{0} tests failed. Dumping results to {1}.".format(len(tests_failed), args.errors_file.name))
-    json.dump(result, args.errors_file, indent=4, separators=(',', ': ')) # Pretty-print the output
-
-
-# Close up shop
-alg_collection.close()
+    logging.warning("{0} tests failed. Dumping results to {1}.".format(len(tests_failed), args.errors_file))
+    with open(args.errors_file, 'w') as errors_file:
+        json.dump(result, errors_file, indent=4, separators=(',', ': ')) # Pretty-print the output
